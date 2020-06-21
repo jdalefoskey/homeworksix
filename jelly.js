4c030d8e75c21fe4e2c728ea5801e17bcc7c469c
Dale Foskey <dalefoskey@icloud.com>
2020-06-21 15:50:37 -0400

    updated homework

--- jelly.js
+++ jelly.js
@@ -11,7 +11,7 @@ function myFunction() {
 	if (retrieveHistory2 == null) {
 		retrieveHistory2 = [];
 	}
-	
+
 	//grab the input block
 	let searchCity = document.getElementById('searchBar');
 	searchedCity = searchCity.value;
@@ -19,9 +19,42 @@ function myFunction() {
 	retrieveHistory2.unshift(searchCity.value);
 	//store array in local storage as strings and then convert them back
 	localStorage.setItem('history', JSON.stringify(retrieveHistory2));
-	//update the div with changes 
+	//update the div with changes
 	showHistory();
+
+	// saving api key as a variable for easier access
+	var apikey = 'e4603c56717d371bf7a914a35065d336';
+
+	//ajax call for single day forcast info
+	$.ajax({
+		url: 'https://api.openweathermap.org/data/2.5/weather?q=' + searchCity.value + '&appid=' + apikey,
+		method: 'GET'
+	}).then(function(response) {
+		var city = response.name;
+		var temp = response.main.temp;
+		temp = Math.floor((temp - 273.15) * 9 / 5 + 32);
+		var humidity = response.main.humidity;
+		var windSpeed = response.wind.speed;
+		var cityj = document.getElementById('city');
+		var tempj = document.getElementById('temp');
+		var humj = document.getElementById('humidity');
+		var windj = document.getElementById('windSpeed');
+		cityj.innerHTML = city;
+		tempj.innerHTML = temp;
+		humj.innerHTML = humidity;
+		windj.innerHTML = windSpeed;
+		console.log(response);
+	});
+
+	//ajax call for 5 day forcast info
+	$.ajax({
+		url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchCity.value + '&appid=' + apikey,
+		method: 'GET'
+	}).then(function(response) {
+		//console.log(response.main);
+	});
 }
+
 // this function will create a new div with each history city added
 function showHistory() {
 	var retrieveHistory = localStorage.getItem('history');
@@ -29,7 +62,7 @@ function showHistory() {
 	if (retrieveHistory2 == null) {
 		return;
 	}
-	//grabbing element and empty the history div 
+	//grabbing element and empty the history div
 	document.getElementById('history').innerHTML = '';
 	for (i = 0; i < retrieveHistory2.length; i++) {
 		$(`<div class="historyCity" />`).text(retrieveHistory2[i]).appendTo('#history');
